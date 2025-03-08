928dcc26647dc5490c0d3da777bf2b5d3675d77e
achmichael <achmadmichael4@gmail.com>
2025-03-09 01:20:40 +0700

    fixing files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        \App\Models\Role::create([
+            'role_name'   => 'admin',
+            'description' => 'Administrator',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        \App\Models\Role::create([
+            'role_name'   => 'company',
+            'description' => 'Company',
+        ]);
+        
+        \App\Models\Role::create([
+            'role_name'   => 'freelancer',
+            'description' => 'Freelancer',
+        ]);
+
+        \App\Models\Role::create([
+            'role_name'   => 'government',
+            'description' => 'Government',
+        ]);
+
+        \App\Models\Role::create([
+            'role_name'   => 'client',
+            'description' => 'Individual',
+        ]); 
+
+        $user = \App\Models\User::create([
+            'role_id'   => 1,
+            'username'  => 'admin',
+            'email'     => 'admin@gmail.com',
+            'password'  => bcrypt('password'),
+        ]);
+
+        $category = \App\Models\Category::create([
+            'category_name' => 'Web Development',
+            // 'description' => 'Create a website or web application for a business or personal use case scenario, with a focus on functionality, user experience, and performance. and great backend service to make application is rapidly',
+        ]);
+
+        \App\Models\Post::create([
+            'title' => 'Web Development',
+            'description' => 'Create a website or web application for a business or personal use case scenario, with a focus on functionality, user experience, and performance. and great backend service to make application is rapidly',
+            'price' => 1000000,
+            'required_skills' => ["PHP", "Laravel", "JavaScript", "VueJS"],
+            'min_experience_years' => 2,
+            'category_id' => $category->id,
+            'number_of_employee' => 2,
+            'posted_by' => $user->id,
         ]);
     }
 }
